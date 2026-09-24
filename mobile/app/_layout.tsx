2527699a4dcdefea7eaf4113462c97cf27879ff0
zzjw0611 <101614767+zzjw0611@users.noreply.github.com>
2026-09-24 14:26:11 +0800

    feat: rename app and apply selected round-table icon

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -7,7 +7,7 @@ import { colors } from '../src/ui';
 
 export default function Layout() {
   return <SafeAreaProvider><SessionProvider><StatusBar style="light" /><Stack screenOptions={{ headerStyle: { backgroundColor: colors.bg }, headerTintColor: colors.ink, headerShadowVisible: false, contentStyle: { backgroundColor: colors.bg } }}>
-    <Stack.Screen name="index" options={{ title: 'AVALON', headerTitleStyle: { color: colors.gold } }} />
+    <Stack.Screen name="index" options={{ title: 'man姐想玩阿瓦隆', headerTitleStyle: { color: colors.gold } }} />
     <Stack.Screen name="create" options={{ title: '创建房间' }} />
     <Stack.Screen name="room/[code]" options={{ title: '阿瓦隆对局' }} />
   </Stack></SessionProvider></SafeAreaProvider>;
